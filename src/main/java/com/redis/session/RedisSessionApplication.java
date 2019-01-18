b88660ac0726cda27f1933f5a4f61e21a311c3b5
Eniac <Eniac@Eniac>
2019-01-17 23:01:37 -0600

    e agrega comentario de tuto

--- src/main/java/com/redis/session/RedisSessionApplication.java
+++ src/main/java/com/redis/session/RedisSessionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedisSessionApplication {
 //https://github.com/MicrosoftArchive/redis/releases
-	
+//https://docs.spring.io/spring-session/docs/current/reference/html5/guides/boot-redis.html
 	public static void main(String[] args) {
 		SpringApplication.run(RedisSessionApplication.class, args);
 	}
